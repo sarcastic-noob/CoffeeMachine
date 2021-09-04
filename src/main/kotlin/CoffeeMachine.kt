c6b052d48e11fda2657fecbf45fd8eb3e391c2cc
Pratyush Singh <pratyush@lastbrand.com>
2021-09-04 22:59:20 +0530

    added application runner

---
+++ src/main/kotlin/CoffeeMachine.kt
@@ -0,0 +1,18 @@
+import com.beust.klaxon.Klaxon
+import domain.MachineHolder
+import service.CoffeeMachineService
+import java.io.File
+
+class CoffeeMachine {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            val initializationFilePath = "src/main/resources/input_1.json"
+            val initializationFile = File(initializationFilePath)
+            val machineHolder = Klaxon().parse<MachineHolder>(initializationFile)!!
+            val machineService = CoffeeMachineService()
+            machineService.init(machineHolder.machine.outlets.count, machineHolder.machine.inventory)
+            machineService.processInput(machineHolder.machine.getBeveragesFromRequest())
+        }
+    }
+}
